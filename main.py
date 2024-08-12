71ef281b1af4c312015f87b9d10aa3b9841f2270
griffing52 <emailgriffinnow@gmail.com>
2024-08-11 21:35:01 -0700

    Working algorithm

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import random
 from util import mp3FromPhrase, textToSpeech, deleteFilesInFolder, joinAudio
-import random
+from pydub import AudioSegment
+from functools import reduce
+
+import os
+
+from util import Word, Phrase, Audio
 
 random.seed(0)
 
@@ -17,57 +22,143 @@ starters = ["starter1", "starter2"]
 currDir = 'C:/Users/griff/Documents/Programming/Python/SwissGermanLanguage/'
 audioDir = currDir + "audio/"
 
-phrases = []
+def compile(path: str, wordPath: str = "words.txt"):
+    wordDict = {}
+    phrases = []
 
-def compile(path: str):
+    # TODO make sure to increase everytime a word/phase is written
+    time = 0
+
+    with open(wordPath, 'r') as wordFile:
+        lines = wordFile.readlines()
+        for line in lines:
+            foreign, english = line.split("=")
+            # remove new line
+            english = english[:-1]
+            wordDict[foreign] =  Word(foreign, english)
+            textToSpeech(english, f"translated_{foreign}")
+
     chapterName = path.split('.')[0]
-    with open(f"{currDir}output/{chapterName}.order", 'a') as order:
-        with open(path, 'r') as file:
-            lines = file.readlines()
-            lineNumber = 0
-            # look through each line
-            for line in lines:
-                # inject mp3
-                if (line.startswith('$')):
-                    write_line(order, line[1:-1])
-                # define word/phrase
-                elif (line.startswith('|')):
-                    try:
-                        line.index('=')
-                    except ValueError:
-                        raise Exception(f"No '=' found in line {lineNumber}")
+    
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        lineNumber = 0
+        # look through each line
+        for line in lines:
+            # inject mp3
+            if (line.startswith('$')):
+                phrases.append(Audio(line[1:-1]))
+                # write_line(order, line[1:-1])
+            # define word/phrase
+            elif (line.startswith('|')):
+                try:
+                    line.index('=')
+                except ValueError:
+                    raise Exception(f"No '=' found in line {lineNumber}")
 
-                    # Swiss German phrase    
-                    phrase = line[1:line.index('=')]
-                    # English phrase
-                    translation = line[line.index('=')+1:]
+                # Swiss German phrase    
+                phrase = line[1:line.index('=')]
+                # English phrase
+                translation = line[line.index('=')+1:]
 
-                    # introduce the phrase
-                    write_line(order, f"global/{random.choice(intros)}")  
-                    textToSpeech(translation, f"translate_{phrase}")
-                    write_line(order, f"/generated/translate_{phrase}")
+                phraseFileName = phrase.replace(' ','-')
+                # introduce the phrase
+                # write_line(order, f"global/{random.choice(intros)}")  
+                if not os.path.isfile(f"{currDir}audio/generated/translated_{phraseFileName}.mp3"):
+                    textToSpeech(translation, f"translated_{phraseFileName}")
 
-                    try:
-                        phrase.index('-')
-                        write_line(order, f"generated/{mp3FromPhrase(phrase)}")
-                    except ValueError:
-                        write_line(order, f"words/{phrase}")
+                if not os.path.isfile(f"{currDir}audio/generated/{phraseFileName}.mp3"):
+                    mp3FromPhrase(phraseFileName)
+                # write_line(order, f"/generated/translate_{phrase}")
 
-                    phrases.append({"english": translation, "audio": phrase, "introduced": False, "dependencies": phrase.split('-'), "mentioned": 0})
-                # text to speech comment
-                elif (line.startswith('#')):
-                    textToSpeech(line[1:], f"comment{lineNumber}")
-                    write_line(order, f"/generated/comment{lineNumber}")
+                # try:
+                #     phrase.index('-')
+                #     write_line(order, f"generated/{mp3FromPhrase(phrase)}")
+                # except ValueError:
+                #     write_line(order, f"words/{phrase}")
+
+                phrases.append(Phrase(phrase, translation))
                 
-                lineNumber += 1
+            # text to speech comment
+            elif (line.startswith('#')):
+                textToSpeech(line[1:], f"comment{lineNumber}")
+                phrases.append(Audio(f"generated/comment{lineNumber}"))
+                # write_line(order, f"/generated/comment{lineNumber}")
+            
+            lineNumber += 1
 
-    for phrase in phrases:
-        print(phrase)
+    with open(f"{currDir}output/{chapterName}.order", 'a') as order:
+        for phrase in phrases:
+            # inject mp3 and skip the rest if pure audio file (comments and injected mp3s)
+            if (phrase.complexity == 0):
+                write_line(order, phrase.getAudioFile())
+                continue
+            
+            writeWordAudioSegment(order, phrase, time, intros)
+            # write_line(order, f"global/{random.choice(intros)}")
+            # write_line(order, f"generated/translated_{phrase.getFileName()}")
+            # write_line(order, phrase.getAudioFile())
+            time += 1
+
+            dependenciesPassed = 0
+
+            while dependenciesPassed < len(phrase.dependencies):
+                dependenciesPassed = 0
+                for word in phrase.dependencies:
+                    if word not in wordDict:
+                        raise Exception(f"Word {word} not found in dictionary")
+
+                    if wordDict[word].rep >= phrase.complexity and time - wordDict[word].age < OLD_AGE:
+                        dependenciesPassed += 1
+                        continue
+
+                    # probability of choosing word gets lower the more it's repeated
+                    # 0 -> 1
+                    # 1 -> 0.77
+                    # 2 -> 0.63
+                    # 3 -> 0.53
+                    prob = 1 / (0.35 * wordDict[word].rep + 1)
+
+                    # skip word if not chosen
+                    if not random.random() < prob:
+                        continue
+
+                    if wordDict[word].rep == 0:
+                        time += 1
+                        writeWordAudioSegment(order, wordDict[word], time, intros)
+                        # wordDict[word].rep += 1
+                        # write_line(order, f"global/{random.choice(intros)}") 
+                        # write_line(order, f"generated/translated_{wordDict[word].getFileName()}")
+                        # write_line(order, wordDict[word].getAudioFile())
+
+                    time += 1
+                    writeWordAudioSegment(order, wordDict[word], time)
+                    # wordDict[word].rep += 1
+                    # write_line(order, f"global/{random.choice(starters)}") 
+                    # write_line(order, wordDict[word].getTranslatedAudioFile())
+                    # write_line(order, wordDict[word].getAudioFile())
+                    # wordDict[word].age = time
+
+
+            time += 1
+            writeWordAudioSegment(order, phrase, time)
+            # write_line(order, f"global/{random.choice(starters)}") 
+            # write_line(order, phrase.getTranslatedAudioFile())
+            # write_line(order, phrase.getAudioFile())
+            # phrase.rep += 1
 
     return chapterName
 
+
+def writeWordAudioSegment(file, audio: Audio, time, preAudio = starters):
+    audio.rep += 1
+    write_line(order, f"global/{random.choice(preAudio)}") 
+    write_line(order, audio.getTranslatedAudioFile())
+    write_line(order, audio.getAudioFile())
+    audio.age = time
+
 def build(chapterName):
-    with open (f"audio/generated/{chapterName}.order", 'r') as file:
+    with open (f"output/{chapterName}.order", 'r') as file:
         # get audio segments in order
         audios = [AudioSegment.from_mp3(audioDir + line.replace("\n", ".mp3")) for line in file.readlines()]
 
@@ -84,5 +175,7 @@ def clean():
 def write_line(file, line):
     file.write(line + "\n")
 
-build(compile("chapter1.txt"))
-clean()
+# build(compile("lessons.txt"))
+build("lessons")
+# compile("lessons.txt")
+# clean()
--- util.py
+++ util.py
@@ -7,6 +7,62 @@ import os, shutil
 
 audioDir = 'C:/Users/griff/Documents/Programming/Python/SwissGermanLanguage/audio/'
 
+class Audio:
+    def __init__(self, filename):
+        self.filename = filename
+        self.complexity = 0
+        self.rep = 0 # how many times the word has been repeated
+        self.age = 0 # the last time the word was repeated
+
+    def getFileName(self):
+        return self.filename
+
+    def getAudioFile(self):
+        return f"{self.filename}"
+
+    def getTranslatedAudioFile(self):
+        return f"generated/translated_{self.filename}"
+
+    def __str__(self):
+        return f"{self.filename}"
+
+    def __repr__(self):
+        return self.filename
+
+class Word(Audio):
+    def __init__(self, word, translation):
+        Audio.__init__(self, word)
+        self.word = word
+        self.translation = translation
+
+    def getAudioFile(self):
+        return f"words/{self.filename}"
+
+    def __str__(self):
+        return f"{self.word}, rep: {self.rep}, age: {self.age}" 
+
+    def __repr__(self):
+        return f"{self.word} - {self.translation}"
+
+class Phrase(Audio):
+    def __init__(self, phrase, translation):
+        self.phrase = phrase
+        self.translation = translation
+        self.dependencies = phrase.split(' ')
+        Audio.__init__(self, '-'.join(self.dependencies))
+
+        self.complexity = len(self.dependencies) + 1
+
+    def getAudioFile(self):
+        return f"generated/{self.filename}"
+
+    def __str__(self):
+        return f"{self.phrase}, {self.complexity}"
+
+    def __repr__(self):
+        return f"{self.phrase} - {self.translation}"
+
+# TODO Dependent phrases?
 # mp3FromPhrase("mier-gond-weil-mier-luscht-hend-uf-Pizza-und-SpaghettiBolognese")
 def mp3FromPhrase(phrase: str):
     audioFiles = [AudioSegment.from_mp3(f"{audioDir}words/{word}.mp3") for word in phrase.split('-')]
@@ -23,7 +79,7 @@ def textToSpeech(phrase: str, fileName: str):
     audio = gTTS(text=phrase, lang='en', slow=False)
 
     # Saving the converted audio in a mp3 file named
-    audio.save(f"{audioDir}/generated/{fileName}.mp3")
+    audio.save(f"{audioDir}generated/{fileName}.mp3")
 
 
 def joinAudio(audios: list, crossfade: int, silence: int):
